e139abb08f895a3841b52f2399d1a894bf79bd77
marti7734 <m.ivanov2018@pmg-pernik.com>
2023-01-23 13:36:52 +0200

    DogAplication

---
+++ WebApplication1/WebApplication1/Controllers/DogsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class DogsController : Controller
+
+    {
+        private readonly ApplicationDbContext context;
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+    
+    
+}
